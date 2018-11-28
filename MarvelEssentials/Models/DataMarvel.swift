f6bbc614b4900197975d61f07d5107ae9a30d447
Nigel Mestach <nigel.mestach.y7064@student.hogent.be>
2018-11-28 14:53:13 +0100

    comment added on why i use struct

--- MarvelEssentials/Models/DataMarvel.swift
+++ MarvelEssentials/Models/DataMarvel.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-
+//Reden om overal structs te gebruiken:  https://medium.com/swift-programming/swift-caveats-for-structs-as-data-models-8299d84b49dc
 struct DataMarvel : Decodable {
     var data : CharacterDataContainer
     var code : Int
